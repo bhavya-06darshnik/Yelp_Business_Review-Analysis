399730a36b422bc731eb008318f1a836e6245081
Bhavya <bhavyaky0690@gmail.com>
2025-06-03 17:09:14 +0530

    Writing 10 sql queries.

---
+++ 10 SQL QUESTIONS.sql
@@ -0,0 +1,22 @@
+-- 1. find number of businesses in each category.
+
+with cte as (
+    select business_id, trim(A.value) as category          --as we know categorie are given in comma separated way
+    from tbl_yelp_businesses,
+         lateral split_to_table(categorie, ',') A
+    )
+select category,count(*) as no_of_businesses
+from cte
+group by 1
+order by 2 desc    --Restaurants is maximum as no. of businesses
+
+
+--2. Find top 10 users who have reviewd the most businesses in 'Restaurants' category.
+
+select r.user_id, count(distinct r.business_id)
+from tbl_yelp_reviews r
+         inner join tbl_yelp_businesses b on r.business_id=b.business_id
+where b.categorie ilike '%restaurant%'
+group by 1
+order by 2 desc
+limit 10
